fe8bb54c19b9862f2cdb29f8e87e497e7dde8b19
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2025-11-20 15:54:17 +0530

    msg

--- basic.java
+++ basic.java
@@ -1,7 +1,32 @@
-class basic{
+import java.util.Scanner; 
 
-    public static void main( String [] arr){
+class basic
+{
+    public static void main (String  []arr){
 
-        System.out.print("I'm Muskan Parmar");
+        // int money = 200;                              //variable
+        // String name = "muskan";
+        // System.out.println(money);
+        // System.out.print(name);
+
+
+        // boolean flag = false;                             //boolean  datatype
+        // byte b = 60;                                      //byte  datatype
+        // short s = 1000;                                   //short  datatype
+        // int a = 1;                                        //int   datatype
+        // long m =  23729083205;                            //long  datatype
+        // char A = 'p';                                     //character datatype
+        // float f = 86.170;                                 //float  datatype
+        // double d = 230283.0239;                           //double  datatype
+    
+
+       //input
+       Scanner sc = new Scanner(System.in);
+
+       System.out.println("Enter your num:");
+       int num = sc.nextInt();
+
+       System.out.print(num);
+       
     }
-}10[p[p]]
+}
